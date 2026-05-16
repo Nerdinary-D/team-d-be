2d8bc0ce24f6f839cc6f7ff38649a4e6cff0dfc8
kjh015 <khj843@naver.com>
2026-05-17 04:57:56 +0900

    feat: Mate API 구현

--- src/main/java/com/dteam/neordinarydteam/domain/partnerpost/dto/request/PartnerPostRequest.java
+++
@@ -1,9 +0,0 @@
-package com.dteam.neordinarydteam.domain.partnerpost.dto.request;
-
-public final class PartnerPostRequest {
-    private PartnerPostRequest() {
-        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
-    }
-
-    public record CreateDTO() {}
-}
