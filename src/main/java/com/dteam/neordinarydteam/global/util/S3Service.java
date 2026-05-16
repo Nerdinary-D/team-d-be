08dc4d16430dceb64b615359f7c2658f111c83d9
yeobi <kimhyjjang@gmail.com>
2026-05-17 01:34:38 +0900

    feat: s3 에러 처리 수정

--- src/main/java/com/dteam/neordinarydteam/global/util/S3Service.java
+++ src/main/java/com/dteam/neordinarydteam/global/util/S3Service.java
@@ -1,14 +1,18 @@
 package com.dteam.neordinarydteam.global.util;
 
+import com.dteam.neordinarydteam.domain.facility.exception.FacilityException;
+import com.dteam.neordinarydteam.domain.facility.exception.code.FacilityErrorCode;
 import java.io.IOException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 
 @Service
 @RequiredArgsConstructor
@@ -33,8 +37,8 @@ public class S3Service {
                     .build();
 
             s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
-        } catch (IOException e) {
-            throw new RuntimeException("파일 업로드에 실패했습니다.", e);
+        } catch (IOException | S3Exception | SdkClientException e) {
+            throw new FacilityException(FacilityErrorCode.IMAGE_UPLOAD_FAILED);
         }
 
         return String.format("https://%s.s3.%s.amazonaws.com/%s", bucket, region, fileName);
