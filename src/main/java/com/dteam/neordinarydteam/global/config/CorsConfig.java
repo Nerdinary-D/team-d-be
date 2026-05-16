664c09f1a9f763374de30171bd392823252b1e37
yeobi <kimhyjjang@gmail.com>
2026-05-17 07:22:08 +0900

    chore: cors config 설정

---
+++ src/main/java/com/dteam/neordinarydteam/global/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.dteam.neordinarydteam.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3000", "http://localhost:5173", "https://all-ground.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
